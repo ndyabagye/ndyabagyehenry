204c46885edf32804906016455ca35cb134456a1
ndyabagye <ndyabagyehenrytusi@gmail.com>
2025-03-10 14:41:50 +0300

    Add Instrumement sans font, add new footer components, link to visit project url on show project detail page, fix flickering on pages on dark mode

--- content/collections/pages/home.md
+++ content/collections/pages/home.md
@@ -5,12 +5,12 @@ title: Home
 template: home
 author: 7f2e6803-269b-43ed-84dc-a2063dcacdfb
 updated_by: 7f2e6803-269b-43ed-84dc-a2063dcacdfb
-updated_at: 1741510169
+updated_at: 1741605013
 ---
-<h1
-                        class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl font-henry">
-                        Hey there, I'm Ndyabagye Henry!</h1>
-                    <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">I’m a Business Analyst at GDExperts,
-                        developer, part time UI designer. I use the
-                        <abbr title="React.js, Inertia, Laravel &amp; Tailwind">RILT</abbr> stack.
-                    </p>
+<h1 class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl font-henry">
+	Hey there, I'm Ndyabagye Henry!
+</h1>
+<p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+	I’m a Business Analyst at GDExperts, developer, part time UI designer. I use the
+    <abbr title="React.js, Inertia, Laravel &amp; Tailwind">RILT</abbr> stack.
+</p>
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,38 +1,38 @@
 // This is all you.
 
 // Dark mode toggle
-document.addEventListener("DOMContentLoaded", function () {
-	const themeToggleButton = document.getElementById("theme-toggle");
-	const themeIcon = document.getElementById("theme-icon");
-    const root = document.documentElement;
+// document.addEventListener("DOMContentLoaded", function () {
+// 	const themeToggleButton = document.getElementById("theme-toggle");
+// 	const themeIcon = document.getElementById("theme-icon");
+//     const root = document.documentElement;
 	
-    // Function to set the theme
-    function setTheme(theme) {
-		if (theme === "dark") {
-			root.classList.add("dark");
-			localStorage.setItem("theme", "dark");
-			themeIcon.textContent = "🌙"; // Moon icon for dark mode
-        } else {
-            root.classList.remove("dark");
-			localStorage.setItem("theme", "light");
-			themeIcon.textContent = "🌞"; // Sun icon for light mode
-        }
-    }
+//     // Function to set the theme
+//     function setTheme(theme) {
+// 		if (theme === "dark") {
+// 			root.classList.add("dark");
+// 			localStorage.setItem("theme", "dark");
+// 			themeIcon.textContent = "🌙"; // Moon icon for dark mode
+//         } else {
+//             root.classList.remove("dark");
+// 			localStorage.setItem("theme", "light");
+// 			themeIcon.textContent = "🌞"; // Sun icon for light mode
+//         }
+//     }
 	
-    // Check localStorage for theme preference
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) {
-		setTheme(storedTheme);
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-		setTheme("dark");
-    }
+//     // Check localStorage for theme preference
+//     const storedTheme = localStorage.getItem("theme");
+//     if (storedTheme) {
+// 		setTheme(storedTheme);
+//     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+// 		setTheme("dark");
+//     }
 	
-    // Event listener for the toggle button
-    themeToggleButton.addEventListener("click", function () {
-		if (root.classList.contains("dark")) {
-			setTheme("light");
-        } else {
-			setTheme("dark");
-        }
-    });
-});
+//     // Event listener for the toggle button
+//     themeToggleButton.addEventListener("click", function () {
+// 		if (root.classList.contains("dark")) {
+// 			setTheme("light");
+//         } else {
+// 			setTheme("dark");
+//         }
+//     });
+// });
