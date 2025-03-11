e88d68bd7355ddc432a7ff13afffb07612d886dc
ndyabagye <ndyabagyehenrytusi@gmail.com>
2025-03-11 11:10:36 +0300

    Add site map route and file

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+// Sitemap generator route
+Route::statamic('sitemap.xml', 'sitemap', ['layout' => 'sitemap', 'content_type' => 'xml']);
